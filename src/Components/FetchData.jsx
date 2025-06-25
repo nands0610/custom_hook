036687c4f71bb836d0a02ec97788e74e6de333f9
nands0610 <sn06102005@gmail.com>
2025-06-25 02:23:46 -0400

    Fetch data from api using the custom hook

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import UseFetch from './UseFetch'
 
 const FetchData = () => {
+  const [data]=UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+  console.log(data)
   return (
     <>
      <ul className='list_data_main'>
